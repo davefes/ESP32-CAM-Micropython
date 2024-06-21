34f7c39efe1a892dd1c0ae2e16e9db437791c69d
Dave Festing <78183339+davefes@users.noreply.github.com>
2024-06-21 14:29:09 +1200

    Update main.py

--- main.py
+++ main.py
@@ -21,107 +21,133 @@
 # THE SOFTWARE.
 
 
-# V1 29 Dec 2022
+# V2 21 June 2024
 
+#  get a buffer protocol required error if you don't do a machine.reset()
+#  or a poweron_reset()
+#  flash_light disconnected as putting 4V or 3V3 on the line down the edge
+#  of the board caused 80mA pulses of current.  That 3V3 line is right close
+#  to the UART TX line and maybe some form of oscillation was taking place
+#  not on the 2nd board
 
-
-#  get a buffer protocol required error if you don't do a umachine.reset()
-#  uncomment all the watchdog stuff if you want an extra layer of reliability
+#  21 June 2024 D. Festing
 
 
 import camera
-from umachine import Pin, WDT, lightsleep
-import umachine
-import utime
+from machine import Pin, WDT, lightsleep
+import machine
+import time
 import gc
 import config
 from my_sender import send_alarm
 from mail_image import send_mail
-from wifi_functions import connect
-from wifi_functions import disconnect
+from wifi_functions import connect, disconnect
 import esp32
-#from watchdog_timer import wdt_callback
-#from watchdog_timer import wdt_feed
+#from watchdog import wdt_init, wdt_feed
 
 
-wake_source = Pin(13, Pin.IN) #  setting wake up pin
-esp32.wake_on_ext0(pin = wake_source, level = esp32.WAKEUP_ANY_HIGH)  #  initialising wake up
-flash_light = Pin(4,Pin.OUT)
+gc.collect()
+
+wake_source = Pin(13, Pin.IN)  # setting wake up pin
+esp32.wake_on_ext0(pin = wake_source, level = esp32.WAKEUP_ANY_HIGH)   # initialising wake up
+flash_light = Pin(4,Pin.OUT, hold=False)
+big_flash_light = Pin(15, Pin.OUT, hold=False)
+poweron_reset = Pin(2, Pin.OUT,  hold=False)
+
 flash_light.off()
+big_flash_light.off()
+poweron_reset.off()
 
-
-
-#  Software WDT set for 80 seconds
-#wdt_callback() #  set up for 80 seconds
+# SW/WDT set for 90 seconds
+#wdt_init(90)
 #wdt_feed()
-#  system WDT set for 100 seconds
-#wdt = WDT(timeout = 100000)  # enable it with a 100 second timeout
-#wdt.feed()
+
+# system WDT set for 100 seconds
+#wdt = WDT(timeout = 100000)
+
+if machine.reset_cause() == machine.SOFT_RESET:
+    print('doing a poweron_reset(), ignore Brownout msg')
+
+    poweron_reset.on()
+
+print('waiting 2 seconds so that I can do a CTRL-C')
+time.sleep(2)
 
 
 def main():
+#    wdt_feed()
+#    wdt.feed()
 
     gc.collect()
 
     camera.init()
 
- #  camera settings
-    camera.pixformat(0) #  0:JPEG, 1:Grayscale (2bytes/pixel), 2:RGB565
-    camera.framesize(11) #  1:96x96, 2:160x120, 3:176x144, 4:240x176, 5:240x240
-                         #  6:320x240, 7:400x296, 8:480x320, 9:640x480, 10:800x600
-                         #  11:1024x768, 12:1280x720, 13:1280x1024, 14:1600x1200
-                         #  15:1920x1080, 16:720x1280, 17:864x1536, 18:2048x1536
-    camera.quality(5) #  [0,63] lower number means higher quality
-#    camera.contrast(0) #  [-2,2] higher number higher contrast
-#    camera.saturation(0) #  [-2,2] higher number higher saturation. -2 grayscale
-#    camera.brightness(0) #  [-2,2] higher number higher brightness. 2 brightest
-    camera.speffect(2) #  0:,no effect 1:negative, 2:black and white, 3:reddish,
-                       #  4:greenish, 5:blue, 6:retro
-#    camera.whitebalance(0) #  0:default, 1:sunny, 2:cloudy, 3:office, 4:home
-#    camera.aelevels(0) #  [-2,2] AE Level: Automatic exposure
-#    camera.aecvalue(0) #  [0,1200] AEC Value: Automatic exposure control
-#    camera.agcgain(0)  #  [0,30] AGC Gain: Automatic Gain Control
+  # camera settings
+    camera.pixformat(0)   # 0:JPEG, 1:Grayscale (2bytes/pixel), 2:RGB565
+    camera.framesize(11)  # 1:96x96, 2:160x120, 3:176x144, 4:240x176, 5:240x240
+                          # 6:320x240, 7:400x296, 8:480x320, 9:640x480, 10:800x600
+                          # 11:1024x768, 12:1280x720, 13:1280x1024, 14:1600x1200
+                          # 15:1920x1080, 16:720x1280, 17:864x1536, 18:2048x1536
+    camera.quality(5)  # [0,63] lower number means higher quality
+#    camera.contrast(0)  # [-2,2] higher number higher contrast
+#    camera.saturation(0)  # [-2,2] higher number higher saturation. -2 grayscale
+#    camera.brightness(0)  # [-2,2] higher number higher brightness. 2 brightest
+#    camera.speffect(2)  # 0:,no effect 1:negative, 2:black and white, 3:reddish,
+                        # 4:greenish, 5:blue, 6:retro
+#    camera.whitebalance(0)  # 0:default, 1:sunny, 2:cloudy, 3:office, 4:home
+#    camera.aelevels(0)  # [-2,2] AE Level: Automatic exposure
+#    camera.aecvalue(0)  # [0,1200] AEC Value: Automatic exposure control
+#    camera.agcgain(0)   # [0,30] AGC Gain: Automatic Gain Control
 
-    print ('waiting 5 seconds so you can do a CTRL-C')
-    utime.sleep(5)  # this wait period is also important for camera setup
+    print ('waiting 5 seconds for camera setup')
+    time.sleep(5)
 
 
     while True:
-        print (f'going to lightsleep for 60 seconds')
-        utime.sleep_ms(50)
+        print ('going to lightsleep FOREVER')
+        time.sleep_ms(5)
+
+        flash_light.init(hold=True)
+        big_flash_light.init(hold=True)
+        poweron_reset.init(hold=True)
 
 #        wdt.feed()
 #        wdt_feed()
 
-        lightsleep(60000)
+        lightsleep()
+
+        flash_light.init(hold=False)
+        big_flash_light.init(hold=False)
+        poweron_reset.init(hold=False)
 
 #        wdt.feed()
 #        wdt_feed()
 
+#        if 1 == 1:  # for testing
         if (wake_source.value() == 1):
             print ('you got a valid trigger')
-            utime.sleep_ms(50)
+            time.sleep_ms(5)
 
+            big_flash_light.on()
 
-            flash_light.on()
-            utime.sleep_ms(100) #  takes more than 50ms for the flash to come up
+            time.sleep_ms(250)  # takes more than 100ms for BIG flash to come up
 
             img = camera.capture()
 
-            flash_light.off()
+#            time.sleep_ms(250)  # probably not needed
+
+            big_flash_light.off()
 
             camera.deinit()
 
-#            print (f'{len(img)}') #  see notes
-
-         #  save image
+          # save image
             with open('image.jpeg', 'wb') as outfile:
                 outfile.write(img)
 
-         #  valid jpeg ending
+          # valid jpeg ending
             valid_jpeg = b'\xff\xd9'
 
-         #  now check for the correct ending
+          # now check for the correct ending
             with open('image.jpeg', 'rb') as file:
                 try:
                     file.seek(-2, 2)
@@ -134,47 +160,58 @@ def main():
 
             last_chars = last_line[-2:]
             print (f'Last 2 characters : {last_chars}')
-
+            time.sleep_ms(5)
 
             if (last_chars == valid_jpeg):
-                print (f'you got a jpeg image')
-                utime.sleep(0.1)
-                
+                print ('you got a jpeg image')
+                time.sleep_ms(5)
+
                 gc.collect()
 
-                print ('send alarm and image')
-                utime.sleep(0.1)
+                print ('send alarm')
+                time.sleep_ms(5)
 
 #                wdt_feed()
 #                wdt.feed()
                 connect()
-                utime.sleep(1)
+                time.sleep(1)
                 send_alarm (config.host, config.port)
-                utime.sleep(1)
-                send_mail({'to': 'your_email@gmail.com', 'subject': 'Message from camera', 'text': 'check this out'}, {'bytes' : img, 'name' : 'img.jpeg'})
+                time.sleep(1)
+
+                print ('send image')
+                time.sleep_ms(5)
+
+                send_mail({'to': 'dave.festing@gmail.com', 'subject': 'Message from camera', 'text': 'check this out'}, {'bytes' : img, 'name' : 'img.jpeg'})
                 disconnect()
 
+                print('image sent')
+                time.sleep_ms(10)
+
               # start all over again
-                umachine.reset()
+                print('doing a poweron_reset(), ignore Brownout msg')
+
+                poweron_reset.on()
 
             else:
-                print (f'not a valid jpeg file')
-                utime.sleep(5)
+                print ('not a valid jpeg file')
+                time.sleep(1)
 
-             #  start all over again
-                umachine.reset()
+              # start all over again
+                print('doing a poweron_reset(), ignore Brownout msg')
 
-        else: #  pat the dogs
-            print ('pat the dogs')
-            utime.sleep(0.1)
+                poweron_reset.on()
+
+#        else:  # pat the dogs
+#            print ('pat the dogs')
+#            time.sleep_ms(5)
 
 #            wdt_feed()
 #            wdt.feed()
 
- #  end of while loop
+  # end of while loop
 
- #  if it gets out of the loop
-    umachine.reset()
+  # if it gets out of the loop
+    poweron_reset.on()
 
 
 if __name__ == '__main__':
