957172af600412bac4357a3ec3b2cb36435f54c7
Dave Festing <78183339+davefes@users.noreply.github.com>
2024-07-30 19:48:44 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2022 David Festing
+# Copyright (c) 2024 David Festing
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,214 +21,176 @@
 # THE SOFTWARE.
 
 
-#  27 June 2024
+#  30 June 2024
 
-
-import camera
-from machine import Pin, WDT, lightsleep
+from machine import Pin, lightsleep
 import machine
 import time
-import gc
 import config
+import esp32
+import camera
 from my_sender import send_alarm
 from mail_image import send_mail
 from wifi_functions import connect, disconnect
-import esp32
-#from watchdog import wdt_init, wdt_feed
 
-
-gc.collect()
 
 wake_source = Pin(13, Pin.IN)  # setting wake up pin
-esp32.wake_on_ext0(pin = wake_source, level = esp32.WAKEUP_ANY_HIGH)   # initialising wake up
-flash_light = Pin(4,Pin.OUT, hold=False)
-big_flash_light = Pin(15, Pin.OUT, hold=False)
-poweron_reset = Pin(2, Pin.OUT,  hold=False)
+esp32.wake_on_ext0(pin = wake_source, level = esp32.WAKEUP_ANY_HIGH)  # initialising wake up
+big_flash = Pin(15, Pin.OUT, value=1)  # inverted logic
 
-flash_light.off()
-big_flash_light.off()
-poweron_reset.off()
-
-# SW/WDT set for 90 seconds
-#wdt_init(90)
-#wdt_feed()
-
-# system WDT set for 100 seconds
-#wdt = WDT(timeout = 100000)
-
-if machine.reset_cause() == machine.SOFT_RESET:
-    print('doing a system reset')
+if machine.reset_cause() ==  machine.SOFT_RESET:
+    print('doing a machine.reset()')
     time.sleep_ms(5)
-#    machine.reset()
-    poweron_reset.on()
-    time.sleep_ms(5)  # so the following is not printed
+    machine.reset()
 
-print('\n\nwaiting 2 seconds so that I can do a CTRL-C')
+print('wait 2 seconds to allow a CTRL-C')
 time.sleep(2)
+
+pix = const(17)
+frame = const(18)
+
+# PIXEL FORMAT
+PIXFORMAT = {
+    'RGB565':1,    # 2BPP/RGB565
+    'YUV422':2,    # 2BPP/YUV422
+    'YUV420':3,    # 1.5BPP/YUV420
+    'GRAYSCALE':4, # 1BPP/GRAYSCALE
+    'JPEG':5,      # JPEG/COMPRESSED
+    'RGB888':6,    # 3BPP/RGB888
+    'RAW':7,       # RAW
+    'RGB444':8,    # 3BP2P/RGB444
+    'RGB555':9,    # 3BP2P/RGB555
+}
+
+# FRAME SIZE
+FRAMESIZE = {
+     '96X96':1,   # 96x96
+     'QQVGA':2,   # 160x120
+     'QCIF':3,    # 176x144
+     'HQVGA':4,   # 240x176
+     '240X240':5, # 240x240
+     'QVGA':6,    # 320x240
+     'CIF':7,     # 400x296
+     'HVGA':8,    # 480x320
+     'VGA':9,     # 640x480
+     'SVGA':10,   # 800x600
+     'XGA':11,    # 1024x768
+     'HD':12,     # 1280x720
+     'SXGA':13,   # 1280x1024
+     'UXGA':14,   # 1600x1200
+     'FHD':15,    # 1920x1080
+     'P_HD':16,   # 720x1280
+     'P_3MP':17,  # 864x1536
+     'QXGA':18,   # 2048x1536
+}
+
+n = PIXFORMAT.get('JPEG')
+s = FRAMESIZE.get('UXGA')
+
+import camera  # do this after the above machine.reset()
+
+camera.conf(pix, n)    # set pixelformat
+camera.conf(frame, s)  # set framesize
 
 
 def main():
-#    wdt_feed()
-#    wdt.feed()
-
-  # wait for camera ready
-    for i in range(5):
-        cam = camera.init()
-        print("Camera ready?: ", cam)
-        if cam:
-            break
-        else:
-            time.sleep(2)
-    else:
-        print('Timeout, doing a poweron_reset')
-        time.sleep_ms(5)
-
-        poweron_reset()
-#        machine.reset()
-
-  # camera settings
-    camera.pixformat(0)   # 0:JPEG, 1:Grayscale (2bytes/pixel), 2:RGB565
-    camera.framesize(10)  # 1:96x96, 2:160x120, 3:176x144, 4:240x176, 5:240x240
-                          # 6:320x240, 7:400x296, 8:480x320, 9:640x480, 10:800x600
-                          # 11:1024x768, 12:1280x720, 13:1280x1024, 14:1600x1200
-                          # 15:1920x1080, 16:720x1280, 17:864x1536, 18:2048x1536
-    camera.quality(5)  # [0,63] lower number means higher quality
-#    camera.contrast(0)  # [-2,2] higher number higher contrast
-#    camera.saturation(0)  # [-2,2] higher number higher saturation. -2 grayscale
-#    camera.brightness(0)  # [-2,2] higher number higher brightness. 2 brightest
-#    camera.speffect(2)  # 0:,no effect 1:negative, 2:black and white, 3:reddish,
-                        # 4:greenish, 5:blue, 6:retro
-#    camera.whitebalance(0)  # 0:default, 1:sunny, 2:cloudy, 3:office, 4:home
-#    camera.aelevels(0)  # [-2,2] AE Level: Automatic exposure
-#    camera.aecvalue(0)  # [0,1200] AEC Value: Automatic exposure control
-#    camera.agcgain(0)   # [0,30] AGC Gain: Automatic Gain Control
-
-  # camera seems to need some more time to setup after changing the settings
-    print('camera setting-up')
-    time.sleep(5)
-
-
     while True:
-        print ('going to lightsleep FOREVER')
+        print ('going to lightsleep')
         time.sleep_ms(5)
 
-        flash_light.init(hold=True)
-        big_flash_light.init(hold=True)
-        poweron_reset.init(hold=True)
+        big_flash.init(hold=True)
 
-#        wdt.feed()
-#        wdt_feed()
+        lightsleep()  # FOREVER
 
-        lightsleep()
+        big_flash.init(hold=False)
 
-# if using watchdogs ... lightsleep(60000)
-
-        flash_light.init(hold=False)
-        big_flash_light.init(hold=False)
-        poweron_reset.init(hold=False)
-
-#        wdt.feed()
-#        wdt_feed()
-
-#        if 1 == 1:  # for testing
-        if (wake_source.value() == 1):
-            print ('you got a valid trigger')
-
-          # wait for vehicle to get in the right position
-            time.sleep_ms(500)
-
-            big_flash_light.on()
-
-          # wait for flash to come up
-            time.sleep_ms(250)
-
-            img = camera.capture()
-
-            time.sleep_ms(100)
-
-            big_flash_light.off()
-
-            camera.deinit()
-
-          # save image
-            with open('image.jpeg', 'wb') as outfile:
-                outfile.write(img)
-
-          # valid jpeg ending
-            valid_jpeg = b'\xff\xd9'
-
-          # now check for the correct ending
-            with open('image.jpeg', 'rb') as file:
-                try:
-                    file.seek(-2, 2)
-                    while file.read(1) != b'\n':
-                        file.seek(-2, 1)
-                except Exception as e:
-                    file.seek(0)
-
-                last_line = file.readline()
-
-            last_chars = last_line[-2:]
-            print (f'Last 2 characters : {last_chars}')
+      # wait for camera ready
+        for i in range(5):
+            cam = camera.init()
+            print("Camera ready?: ", cam)
+            if cam:
+                break
+            else:
+                time.sleep(1)
+        else:
+            print('Timeout, doing a machine.reset()')
             time.sleep_ms(5)
 
-            if (last_chars == valid_jpeg):
-                print ('you got a jpeg image')
-                time.sleep_ms(5)
+            machine.reset()
 
-                gc.collect()
+      # other settings after camera.init()
+        camera.quality(5)
 
-                print ('send alarm')
-                time.sleep_ms(5)
+        big_flash.off()  # inverted logic
+        time.sleep_ms(100)  # wait for flash to warm-up
 
-#                wdt_feed()
-#                wdt.feed()
-                connect()
-                time.sleep(1)
-                send_alarm (config.host, config.port)
-                time.sleep(1)
+      # take 5 to 10 frames, rejecting the first 4 or 9!!
+        for x in range(5):
+            img = camera.capture()
+            time.sleep_ms(50)
 
-                print ('send image')
-                time.sleep_ms(5)
+        print('picture taken')
+        time.sleep_ms(10)
 
-                send_mail({'to': 'dave.festing@gmail.com', 'subject': 'Message from camera', 'text': 'check this out'}, {'bytes' : img, 'name' : 'img.jpeg'})
-                disconnect()
+        big_flash.on()  # inverted logic
 
-                print('image sent')
-                time.sleep_ms(5)
+        camera.deinit()
 
-              # start all over again
-                print('doing a system reset')
-                time.sleep_ms(5)
-#                machine.reset()
-                poweron_reset.on()
-                time.sleep(5)
-            else:
-                print ('not a valid jpeg file')
-                time.sleep_ms(5)
+      # save image
+        with open('image.jpeg', 'wb') as outfile:
+            outfile.write(img)
 
-              # start all over again
-                print('doing a system reset')
-                time.sleep_ms(5)
-#                machine.reset()
-                poweron_reset.on()
-                time.sleep_ms(5)
+      # valid jpeg ending
+        valid_jpeg = b'\xff\xd9'
 
-#        else:  # pat the dogs
-#            print ('pat the dogs')
-#            time.sleep_ms(5)
+      # now check for the correct ending
+        with open('image.jpeg', 'rb') as file:
+            try:
+                file.seek(-2, 2)
+                while file.read(1) != b'\n':
+                    file.seek(-2, 1)
+            except Exception as e:
+                file.seek(0)
 
-#            wdt_feed()
-#            wdt.feed()
+            last_line = file.readline()
+
+        last_chars = last_line[-2:]
+#        print (f'Last 2 characters : {last_chars}')
+        time.sleep_ms(5)
+
+        if (last_chars == valid_jpeg):
+#            print ('you got a jpeg image')
+            time.sleep_ms(5)
+
+            gc.collect()
+
+            connect()
+            time.sleep(1)
+
+            print ('send alarm')
+            time.sleep_ms(5)
+
+            send_alarm (config.host, config.port)
+            time.sleep(1)
+
+            print ('send image')
+            time.sleep_ms(5)
+
+            send_mail({'to': 'xyz@gmail.com', 'subject': 'Message from gate camera', 'text': 'check this out'}, {'bytes' : img, 'name' : 'img.jpeg'})
+
+            disconnect()
+
+            print('image sent')
+            time.sleep_ms(5)
+
+        else:
+            print ('not a valid jpeg file')
+            time.sleep_ms(5)
+
+            machine.reset()
 
   # end of while loop
-
-  # if it gets out of the loop, do a system reset
-    time.sleep_ms(5)
-#    machine.reset()
-    poweron_reset.on()
-    time.sleep_ms(5)
-
-
+  
+            
 if __name__ == '__main__':
     main()
